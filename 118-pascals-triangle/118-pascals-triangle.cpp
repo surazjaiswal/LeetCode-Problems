4c0db35a1458dfbeec2268e613c4387ee9715c54
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-04-22 12:21:08 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.36%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
-    int dp[35][35];
-    vector<vector<int>> generate(int numRows) {
-        memset(dp,-1,sizeof(dp));
-        int n=numRows;
+    vector<vector<int>> generate(int n) {
         vector<vector<int>> ans;
         for(int i=0;i<n;i++){
-            vector<int> vt;
-            for(int j=0;j<=i;j++){
-                vt.push_back(nCr(i,j));
-            }
+            vector<int> vt(i+1,1);
             ans.push_back(vt);
+            for(int j=1;j<i;j++)
+                ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
         }
-        
         return ans;
     }
-    
-    int nCr(int n, int r){
-        if(r==0 || r==n)
-            return 1;
-        if(r==1 || n-r==1)
-            return n;
-        if(dp[n][r]!=-1)
-            return dp[n][r];
-        return dp[n][r]=nCr(n-1,r-1)+nCr(n-1,r);
-    }
-};
+};   
