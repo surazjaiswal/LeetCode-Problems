5c196a558643b5b97ed08a6cd1196e44184005b3
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-06-02 12:33:54 +0530

    Time: 17 ms (42.74%), Space: 10.5 MB (78.16%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -4,30 +4,13 @@ public:
         int row = matrix.size();
         int col = matrix[0].size();
         
-        if(row == col){
-            for(int i=0;i<row;i++){
-                for(int j=i;j<col;j++)
-                    swap(matrix[i][j],matrix[j][i]);
-            }
-            return matrix;
-        }
+        vector<vector<int>> ans(col,vector<int>(row));
         
-        vector<int> nums;
-        for(int i=0;i<col;i++){
-            for(int j=0;j<row;j++){
-                nums.push_back(matrix[j][i]);
-            }
-        }
-        
-        int cnt=0;
-        vector<vector<int>> ans;
-        for(int i=0;i<col;i++){
-            vector<int> vt;
-            for(int j=0;j<row;j++){
-                vt.push_back(nums[cnt++]);
-            }
-            ans.push_back(vt);
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++)
+                ans[j][i] = matrix[i][j];
         }
         return ans;
+        
     }
 };
