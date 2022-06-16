ce0e7dc977a0051c370ab12b22f42be423869a82
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-06-16 13:39:13 +0530

    Time: 505 ms (24.71%), Space: 11.5 MB (55.34%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -9,11 +9,11 @@ public:
         // now the 3rd important case
         // if string size more than 2 
         // then check if first and last char are same or not 
-        // if same then we need to check if string in the mid of both the char are same or not
-        // if same then this complete string will be pallindrome else it will not be
-        // and the import part here is as we progress we are already making check for pallindrome of the middle subtrings
+        // if same then we need to check if string in the middle of both the char are pallindrome or not
+        // if pallindrome then this complete selected string will be pallindrome else it will not be
+        // and the important part here is as we progress we are already making check for pallindrome of the middle subtrings
         // from this we will get the size of largest pallindrome, and starting index
-        // then return the the substring of that size
+        // then return the the substring of that size, with starting index
         
         
         int n = s.length(); // size of string
@@ -24,19 +24,25 @@ public:
         for(int g=0;g<n;g++){
             for(int i=0,j=g;j<n;i++,j++){
                 if(g==0){
+                    // single char is always pallindrome
                     dp[i][j]=1;
                 }else if(g==1){
+                    // if both char are equal then its a pallindrome, else not pallindrome
                     if(s[i] == s[j])
                         dp[i][j]=1;
                     else 
                         dp[i][j]=0;
                 }else{
+                    // if both start & end char are equal and the middle substring is pallindrome then this selected string will pallindrome
                     if(s[i] == s[j] && dp[i+1][j-1])
                         dp[i][j]=1;
+                    // else not pallindrome
                     else
                         dp[i][j]=0;
                 }
                 
+                // if pallindrome, then check length and compare it with previous ans
+                // also upatde the len ans starting index
                 if(dp[i][j]){
                     int sz = j - i + 1;
                     if(sz > len){
@@ -46,6 +52,7 @@ public:
                 }
             }
         }
+        // extracting the required substring
         string ss = s.substr(idx,len);
         return ss;
     }
