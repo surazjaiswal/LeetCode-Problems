2a85946edabcacc57a3b1be33d3a941e18008702
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-06-21 16:36:35 +0530

    Time: 111 ms (63.72%), Space: 10.6 MB (79.38%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        sort(coins.begin(),coins.end());
-        int amt = amount;
-        vector<int> dp(amt+1,amt+1);
-        dp[0]=0;
-        for(int i=0;i<=amt;i++){
-            for(int j=0;j<coins.size();j++){
-                int bal = i - coins[j];
-                if(bal==0){
-                    dp[i]=1;
-                    continue;
-                }else if(bal>0){
-                    dp[i] = min(dp[i],1+dp[bal]);
+    int coinChange(vector<int>& coins, int amt) {
+        int n = coins.size();
+        int dp[n+1][amt+1];
+        
+        // number of coins is zero, so no sum can be build up
+        for(int j=0;j<amt+1;j++)
+            dp[0][j] = INT_MAX - 1; // to avoid any overflow
+        
+        // amount is zero, coins needed is zero
+        for(int i=0;i<n+1;i++)
+            dp[i][0] = 0;
+        
+        // variation of knapsack problem
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<amt+1;j++){
+                if(coins[i-1] <= j){
+                    dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+                }else{
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        if(dp[amt]==amt+1)
+        
+        if(dp[n][amt] > amt)
             return -1;
-        return dp[amt];
+        else 
+            return dp[n][amt];
     }
 };
