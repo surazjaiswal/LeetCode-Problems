370569921250752e7e0bd7bd0f456e9af407518e
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-06-22 03:26:54 +0530

    Time: 161 ms (11.65%), Space: 48.2 MB (5.38%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -1,18 +1,66 @@
+class Node{
+    public:
+    int num;
+    map<int,Node*> children;
+    bool isTerminal;
+    Node(int num){
+        this->num = num;
+    }
+};
+
+class Trie{
+    public:
+    Node *root;
+    
+    Trie(){
+        root = new Node(0);
+    }
+    
+    void insert(int num){
+        Node *temp = root;
+        vector<int> digit;
+        while(num){
+            int r = num%10;
+            digit.push_back(r);
+            num /= 10;
+        }
+        for(int i=digit.size()-1;i>=0;i--){
+            int n = digit[i];
+            if(temp->children.count(n)){
+                temp = temp->children[n];
+            }else{
+                temp->children[n] = new Node(n);
+                temp = temp->children[n];
+            }
+        }
+        temp->isTerminal = true;
+    }
+    
+    vector<int> getNums(){
+        vector<int> ans;
+        int num=0;
+        dfs_helper(root,ans,num);
+        return ans;
+    }
+    
+    void dfs_helper(Node *root,vector<int> &ans,int &num){
+        for(auto it : root->children){
+            num = num*10 + it.first;
+            if(it.second->isTerminal)
+                ans.push_back(num);
+            dfs_helper(it.second,ans,num);
+            num /= 10;
+        }
+    }
+};
+
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
-        vector<int> nums;
+        Trie T;
         for(int i=1;i<=n;i++)
-            nums.push_back(i);
+            T.insert(i);
         
-        sort(nums.begin(),nums.end(),compare);
-        return nums;
-    }
-    
-    bool static compare(int a, int b){
-        string str_a = to_string(a);
-        string str_b = to_string(b);
-        
-        return str_a < str_b;
+        return T.getNums();
     }
 };
