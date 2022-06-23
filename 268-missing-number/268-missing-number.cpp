e21a7aacfe4bbcfd1865cbffdc6d73f31655b2dc
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-06-23 22:35:52 +0530

    Time: 16 ms (94.54%), Space: 18.1 MB (18.89%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        // the idea is using xor operation since xor'ing between two same elements leads to 0 ie nullifying
+        // in xor the order doesn't matter            
+        // ex : 3^(3^0)^(0^1)^(1^2) = 3
+            
         int n = nums.size();
         int ans = n;
+        
+        
         for(int i=0;i<n;i++){
             ans ^= nums[i];
             ans ^= i;
