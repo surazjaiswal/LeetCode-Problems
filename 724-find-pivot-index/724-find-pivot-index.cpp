1f77ee4ca8a67f46256a3ae478dc276ffd5aed77
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-06-25 01:01:14 +0530

    Time: 41 ms (40.17%), Space: 32.2 MB (8.81%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> leftSum(n),rightSum(n);
+        
+        int s=0;
+        for(int i=0;i<n;i++){
+            s+=nums[i];
+            leftSum[i] = s;
+        }
+        
+        s=0;
+        for(int i=n-1;i>=0;i--){
+            s+=nums[i];
+            rightSum[i] = s;
+        }
+        
+        // for(auto it:leftSum)
+        //     cout<<it<<" ";
+        // cout<<endl;
+        // for(auto it:rightSum)
+        //     cout<<it<<" ";
+        // cout<<endl;
+        
+        for(int i=0;i<n;i++){
+            if(leftSum[i]==rightSum[i])
+                return i;
+        }
+        
+        return -1;
+        
+        /*
         int n=nums.size();
         int sum=0;
         for(auto it:nums)
@@ -14,5 +45,6 @@ public:
                 return i;          
         }
         return -1;
+        */
     }
 };
