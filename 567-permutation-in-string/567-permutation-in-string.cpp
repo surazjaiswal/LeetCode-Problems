bf50c9aecb3afd45dcaf3324309d3b3fdd0a4629
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-06-30 20:47:01 +0530

    Time: 14 ms (56.24%), Space: 7.1 MB (91.47%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s1.length() > s2.length())
+        int n1 = s1.size();
+        int n2 = s2.size();
+
+        if(n1 > n2)
             return false;
-        sort(s1.begin(),s1.end());
-        int i=0;
-        while(i <= s2.length() - s1.length()){
-            string str = s2.substr(i,s1.length());
-            sort(str.begin(),str.end());
-            // cout<<str<<" ";
-            if(str == s1)
+
+        vector<int> s1_mp(26,0),s2_mp(26,0);
+        for(int i=0;i<n1;i++){
+            s1_mp[s1[i]-'a']++;
+            s2_mp[s2[i]-'a']++;
+        }
+        
+        if(s1_mp == s2_mp)
+            return true;
+        
+        int i=1,j=n1;
+        while(j < n2){
+            s2_mp[s2[j]-'a']++;
+            s2_mp[s2[i-1]-'a']--;
+            
+            if(s1_mp == s2_mp)
                 return true;
             i++;
+            j++;
         }
-        return false;
+        return false;        
     }
 };
