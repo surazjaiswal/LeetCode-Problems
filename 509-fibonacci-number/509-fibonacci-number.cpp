af11484676ee69568d89d7bcfc9d5b76495f3c7d
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-07-01 20:15:37 +0530

    Time: 3 ms (60.94%), Space: 6 MB (41.38%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int fib(int n) {
-        int dp[50];
-        memset(dp,0,sizeof(dp));
-        for(int i=1;i<=n;i++){
-            if(i<=2){
-                dp[i]=1;
-                continue;
-            }
-            dp[i] = dp[i-1] + dp[i-2];
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        
+        int a = 0, b = 1, c;
+        while((n--) > 1){
+            c = a + b;
+            a = b;
+            b = c;
         }
-        return dp[n];
+        
+        return c;
+        
+        
+        // int dp[50];
+        // memset(dp,0,sizeof(dp));
+        // for(int i=1;i<=n;i++){
+        //     if(i<=2){
+        //         dp[i]=1;
+        //         continue;
+        //     }
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
     }
 };
