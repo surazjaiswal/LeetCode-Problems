610abc6b82543e757480ff1b4e96b36651464523
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-07-03 20:45:18 +0530

    Time: 387 ms (23.63%), Space: 140.7 MB (15.57%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -10,10 +10,11 @@ public:
         }
         
         vector<int> ans;
-        int i=0,j=0;
-        while(i<pos.size() && j<neg.size()){
-            ans.push_back(pos[i++]);
-            ans.push_back(neg[j++]);
+        int i=0; // size of both pos and neg is same, given equal number of positives and negatives
+        while(i<pos.size()){
+            ans.push_back(pos[i]);
+            ans.push_back(neg[i]);
+            i++;
         }
 
         return ans;
