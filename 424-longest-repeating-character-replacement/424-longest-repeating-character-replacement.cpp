3e9f530cc8a8c8d6cccd091a70220bcfaab94e8e
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-07-08 14:29:39 +0530

    Time: 7 ms (91.60%), Space: 7 MB (53.69%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -6,17 +6,21 @@ public:
         int i=0,j=0;
         vector<int> vt(26,0);
         while(j<s.length()){
-            vt[s[j]-'A']++;
+            vt[s[j]-'A']++; // adding the new char in window, also incrementing its freqency count
                         
-            max_count = max(max_count, vt[s[j]-'A']);
+            max_count = max(max_count, vt[s[j]-'A']); // frequency of most frequent char in current window
+            
+            // if window_size - freq_most_freq_char_int this_window is <= k (max_operation)
+            // this difference is the change operation required to make all char same 
+            // the max_window size will be then be obtained
             if((j-i+1) - max_count <= k){
-                max_len = max(max_len, j-i+1);
-                j++;
+                max_len = max(max_len, j-i+1); // max window len till now
+                j++; // moving to next char and thereafter adding it to window in next iteration
             }
             else{
-                vt[s[i]-'A']--;
-                vt[s[j]-'A']--;
-                i++;
+                vt[s[j]-'A']--; // so that it is not counted again in next iteration
+                vt[s[i]-'A']--; // reducing the count of char at ith postion (start of window) as window size is to be reduced;
+                i++;    // now reducing the window size
             }
         }
         
