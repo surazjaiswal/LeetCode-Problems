e91d19d964154a1eccdfec850cd182365187ed33
Suraj Jaiswal <60808596+surazjaiswal@users.noreply.github.com>
2022-07-15 13:16:39 +0530

    4 ms

---
+++ solutions/125. Valid Palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public:
+        bool isPalindrome(string s)
+        {
+            string str = "";
+​
+            for (char c: s)
+            {
+                if (isalpha(c))
+                {
+                    str += tolower(c);
+                }
+                else if (isdigit(c))
+                {
+                    str += c;
+                }
+            }
+​
+            string rev = str;
+            reverse(rev.begin(), rev.end());
+​
+            // cout << str << '\n' << rev << endl;
+​
+            return rev == str;
+        }
+};
